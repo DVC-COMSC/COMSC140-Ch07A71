90cbce66c5f8c64338d6bb7699c59b686a79341a
Kyuwoong Lee <kwooler@gmail.com>
2022-10-01 01:48:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+
+numbers = list(map(int, input().split()))
+
 # ******************************
 # Make your Code
 # ******************************
 
-numbers = list(map(int, input().split()))
+
+# Use this statement to print out the list element. # Replace the variable 'dist' with your variable
+# print (f'{dist:.2f}', end=' ')
